adc48461a6b707da8580ad50f01c0b684a79a45e
vatsals21 <sahayvatsal@gmail.com>
2022-10-25 19:58:21 +0530

    curr_conv update0.1

--- src/components/currencyConverter.js
+++ src/components/currencyConverter.js
@@ -1,34 +1,90 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, TextInput, View, Text} from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
 import {Dropdown} from 'react-native-element-dropdown';
 import {COLORS} from '../assets/colors';
 
 const CurrencyConverter = props => {
   const [currencySymbol, setCurrencySymbol] = useState([]);
+  const [inputNum, setInputNum] = useState(0);
+  const [outputNum, setOutputNum] = useState(0);
+  const [inputCurr, setInputCurr] = useState();
+  const [outputCurr, setOutputCurr] = useState();
+  const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    axios
-      // .get('https://api.adviceslip.com/advice/201')
-      .get('https://api.apilayer.com/exchangerates_data/symbols', {
-        // method: 'GET',
-        // redirect: 'follow',
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.apilayer.com/exchangerates_data/symbols',
+        {
+          headers: {
+            apikey: 'HynEPEUuOZsEe2kLUHaZte6PtwHXJwDZ',
+          },
+        },
+      );
+      var result = response.data.symbols;
+      result = Object.keys(result);
+      var temp_arr = [];
+      for (res in result) {
+        temp_arr = [...temp_arr, {label: result[res], value: result[res]}];
+      }
+      setCurrencySymbol(temp_arr);
+      setInputCurr(temp_arr[0].value);
+      setOutputCurr(temp_arr[0].value);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const convertCurrr = async newNum => {
+    if (isNaN(newNum) || newNum <= 0) {
+      setOutputNum(0.0);
+      return;
+    }
+    try {
+      setLoading(true);
+      const url =
+        'https://api.apilayer.com/exchangerates_data/convert?to=' +
+        outputCurr +
+        '&from=' +
+        inputCurr +
+        '&amount=' +
+        newNum;
+      const response = await axios.get(url, {
         headers: {
           apikey: 'HynEPEUuOZsEe2kLUHaZte6PtwHXJwDZ',
         },
-      })
-      .then(response => {
-        console.log(response);
-        // const res = response.data.symbols;
-        console.log('HELLO');
-        // console.log(res);
-      })
-      .catch(error => {
-        console.log('HELLO - 2');
-        console.log(error);
       });
-    console.log('HELLO - 3');
+      var result = response.data.result;
+      console.log(result);
+      result = +result;
+      setOutputNum(result.toFixed(3));
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
   }, []);
+
+  useEffect(() => {
+    convertCurrr(inputNum);
+  }, [inputCurr]);
+
+  useEffect(() => {
+    convertCurrr(inputNum);
+  }, [outputCurr]);
 
   const styles = StyleSheet.create({
     container: {
@@ -52,37 +108,50 @@ const CurrencyConverter = props => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.container_two}>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={newNum => {
-            console.log(newNum);
-          }}
-          placeholder="0"
-        />
-        <Dropdown
-          style={{backgroundColor: COLORS.beige, height: 40, width: 70}}
-          data={currencySymbol}
-          labelField="label"
-          valueField="value"
-          // value={inputUnit}
-          onChange={item => {
-            // console.log(item);
-          }}
-        />
-      </View>
-      <View style={styles.container_two}>
-        <Text style={styles.textInput}>12</Text>
-        <Dropdown
-          style={{backgroundColor: COLORS.beige, height: 40, width: 70}}
-          data={currencySymbol}
-          labelField="label"
-          valueField="value"
-          // value={outputUnit}
-          dropdownPosition="bottom"
-          onChange={item => {}}
-        />
-      </View>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#00ff00" />
+      ) : (
+        <View>
+          <View style={styles.container_two}>
+            <TextInput
+              style={styles.textInput}
+              // * onChangeText is not a good option here
+              // * as on every digit change, currency will try to change
+              // * thus increasing latency and redundancy for same operation
+              onChangeText={newNum => {
+                setInputNum(newNum);
+                convertCurrr(newNum);
+              }}
+              value={inputNum}
+              placeholder="0"
+            />
+            <Dropdown
+              style={{backgroundColor: COLORS.beige, height: 40, width: 90}}
+              data={currencySymbol}
+              labelField="label"
+              valueField="value"
+              value={inputCurr}
+              onChange={item => {
+                setInputCurr(item.value);
+              }}
+            />
+          </View>
+          <View style={styles.container_two}>
+            <Text style={styles.textInput}>{outputNum}</Text>
+            <Dropdown
+              style={{backgroundColor: COLORS.beige, height: 40, width: 90}}
+              data={currencySymbol}
+              labelField="label"
+              valueField="value"
+              value={outputCurr}
+              dropdownPosition="top"
+              onChange={item => {
+                setOutputCurr(item.value);
+              }}
+            />
+          </View>
+        </View>
+      )}
     </View>
   );
 };
